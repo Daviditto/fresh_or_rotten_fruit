af6a56fff1abe10b4c610451055c945a5828a416
David (Hui) Liu <93046901+Daviditto@users.noreply.github.com>
2023-02-12 10:15:28 -0700

    Add files via upload

---
+++ real_time_mask_detection.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
+import cv2
+
+model = YOLO('weight_path.pt')
+model.predict(source='0', show=True, conf=0.5)
+
+
